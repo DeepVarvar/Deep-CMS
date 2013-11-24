0c2c2b65875b0c82718c2d76a2d878a631ab20a7
DeepVarvar <deepvarvar@mail.ru>
2013-11-25 03:59:02 +0400

    Fix RSS format

--- application/modules/rss_xml/rss_xml.php
+++ application/modules/rss_xml/rss_xml.php
@@ -39,24 +39,45 @@ class rss_xml extends baseController {
             "link"        => $this->mainHostUrl . "/",
             "description" => $rssCnf->tree->description,
             "generator"   => "Deep-CMS " . $config->application->version,
+            "atom:link"   => array(
+                "href" => $this->mainHostUrl . request::getURI()
+            ),
             "item"        => $this->getTreeItems()
         );
 
         view::assign("rss", array(
-            "channel" => array($treeChannel)
+            "channel"   => array($treeChannel)
         ));
 
         view::setXSDSchema(
             array(
                 "name"       => "rss",
                 "attributes" => array(
-                    array("name" => "version", "value" => "2.0")
+                    array(
+                        "name"  => "version",
+                        "value" => "2.0"
+                    ),
+                    array(
+                        "name"  => "xmlns:atom",
+                        "value" => "http://www.w3.org/2005/Atom"
+                    )
                 ),
                 "children" => array(
                     array(
                         "name"     => "channel",
                         "repeat"   => true,
                         "children" => array(
+                            array(
+                                "name" => "atom:link",
+                                "attributes" => array(
+                                    array("name" => "href", "value" => true),
+                                    array("name" => "rel",  "value" => "self"),
+                                    array(
+                                        "name"  => "type",
+                                        "value" => "application/rss+xml"
+                                    )
+                                )
+                            ),
                             array("name" => "title"),
                             array("name" => "link"),
                             array(
@@ -77,19 +98,26 @@ class rss_xml extends baseController {
 
         $items = db::query("
 
-            SET @m= (SELECT (MAX(creation_date) - INTERVAL 1 DAY) FROM tree);
+            SET @m= (SELECT (MAX(creation_date) - INTERVAL 10 DAY) FROM tree);
             SET @n= (NOW());
             SELECT node_name title, CONCAT('%s', page_alias) link,
-                page_text description, creation_date pubDate FROM tree
-                WHERE is_publish = 1 AND in_sitemap_xml = 1
+                page_text description,
+                DATE_FORMAT(
+                    creation_date,'%%a, %%d %%b %%Y %%H:%%i:%%s %s'
+                ) pubDate
+                FROM tree WHERE is_publish = 1 AND in_sitemap_xml = 1
                     AND creation_date BETWEEN @m AND @n
                         ORDER BY creation_date DESC
-                        LIMIT 50", $this->mainHostUrl
+                        LIMIT 30",
+
+                $this->mainHostUrl,
+                str_replace(":", "", app::config()->site->default_timezone)
 
         );
 
         foreach ($items as $k => $item) {
 
+            $items[$k]['guid'] = $items[$k]['link'];
             if (!$item['description']) {
                 $items[$k]['description'] = "[no description]";
                 continue;
